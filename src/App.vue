fd221e03f7676d9865884b87735481dcf8edeee0
cccemail <cccemail@126.com>
2020-10-11 16:34:49 +0800

    项目初始化-目录结构调整

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="less">
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
 </style>
